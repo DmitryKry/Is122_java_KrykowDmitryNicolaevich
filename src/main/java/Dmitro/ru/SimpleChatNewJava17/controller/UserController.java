44585be953f12031a56f06685c547625e0091a5b
DmitryKry <dkyulov@mail.ru>
2025-04-10 12:52:20 +0300

    Снова реализовал пагинацию, так как гит сломал мне коммиты

--- src/main/java/Dmitro/ru/SimpleChatNewJava17/controller/UserController.java
+++ src/main/java/Dmitro/ru/SimpleChatNewJava17/controller/UserController.java
@@ -25,16 +25,20 @@ public class UserController {
     private final UserService userService;
     @GetMapping("/users")
     public String FindAllUsers(@RequestParam(defaultValue = "0") int page,
-                               @RequestParam(defaultValue = "10") int size,
+                               @RequestParam(defaultValue = "1") int size,
                                Model model) {
-        List<User> users = userService.FindAllUsers();
+        List<User> users = new ArrayList<>();
         Page<User> usersPage = userService.FindAllUsers(page, size);
 
+        for (int i = 0; i < 10; i++) {
+            if ((i + page) < userService.FindAllUsers().size())
+                users.add(userService.FindAllUsers().get(i + page));
+        }
         // Если пользователи есть, добавляем их в модель
         if (!users.isEmpty()) {
             model.addAttribute("user", userService.getInMemoryUser());
             model.addAttribute("allUsers", userService.FindAllUsers());
-            model.addAttribute("users", usersPage.getContent());  // Содержимое текущей страницы
+            model.addAttribute("users", users);  // Содержимое текущей страницы
             model.addAttribute("currentPage", page);  // Текущая страница
             model.addAttribute("totalPages", usersPage.getTotalPages());  // Общее количество страниц
             model.addAttribute("totalItems", usersPage.getTotalElements());  // Получаем список пользователей из страницы// Общее количество страниц
